4769972d6f48206b649e4db18143a26327f02df8
afeiship <1290657123@qq.com>
2022-04-29 18:37:27 +0800

    chore: upt *.ts

--- build/scripts.js
+++ build/scripts.js
@@ -9,7 +9,7 @@
 
   gulp.task('scripts:cjs', function () {
     return gulp
-      .src('src/index.ts')
+      .src('src/*.ts')
       .pipe($.replace('export default ', 'export = '))
       .pipe($.jswork.pkgHeader())
       .pipe($.typescript({ ...tsconfig.compilerOptions, module: 'commonjs' }))
@@ -19,7 +19,7 @@
 
   gulp.task('scripts:esm', function () {
     return gulp
-      .src('src/index.ts')
+      .src('src/*.ts')
       .pipe($.jswork.pkgHeader())
       .pipe($.typescript({ ...tsconfig.compilerOptions, module: 'esnext' }))
       .pipe(gulp.dest('dist/esm'))
