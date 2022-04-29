922150222f360b85e5c342abff0bcd00e7513350
afeiship <1290657123@qq.com>
2022-04-29 17:34:10 +0800

    chore: update env-type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import nx from '@jswork/next';
 
 declare var process: any;
+
 type PathType = null | string;
+interface EnvType {
+  [index: string]: string;
+}
 
 const RC_APP = 'REACT_APP_';
 const RC_SIZE = RC_APP.length;
@@ -9,7 +13,7 @@ const RC_SIZE = RC_APP.length;
 class CraEnvs {
   static get(inPath?: PathType, inTarget?) {
     const envs = inTarget || process.env;
-    nx.forIn(envs, (k, v) => {
+    nx.forIn(envs, (k: string, v: EnvType) => {
       if (k.includes(RC_APP)) {
         envs[k.slice(RC_SIZE)] = v;
         delete envs[k];
@@ -21,7 +25,7 @@ class CraEnvs {
   static set(inCmdRc) {
     const envs = inCmdRc;
     nx.forIn(envs, (_: string, value) => {
-      nx.forIn(value, (k, v) => {
+      nx.forIn(value, (k: string, v: EnvType) => {
         if (!k.includes(RC_APP)) {
           value[RC_APP + k] = v;
           delete value[k];
